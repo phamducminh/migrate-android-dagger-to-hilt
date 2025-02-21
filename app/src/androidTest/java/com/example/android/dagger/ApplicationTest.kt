a470abee3d1714cdae969a07cd2b57968af6c303
Minh Pham <pdminh0410@gmail.com>
2025-02-20 21:22:53 -0800

    9. Testing

--- app/src/androidTest/java/com/example/android/dagger/ApplicationTest.kt
+++ app/src/androidTest/java/com/example/android/dagger/ApplicationTest.kt
@@ -18,13 +18,24 @@ package com.example.android.dagger
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.example.android.dagger.main.MainActivity
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
 import org.junit.Test
 
+@HiltAndroidTest
 class ApplicationTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
 
     @Test
     fun runApp() {
--- app/src/androidTest/java/com/example/android/dagger/di/TestStorageModule.kt
+++ app/src/androidTest/java/com/example/android/dagger/di/TestStorageModule.kt
@@ -16,16 +16,16 @@
 
 package com.example.android.dagger.di
 
-import com.example.android.dagger.storage.FakeStorage
-import com.example.android.dagger.storage.Storage
-import dagger.Binds
 import dagger.Module
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
 
 // Overrides StorageModule in android tests
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [StorageModule::class]
+)
 @Module
 abstract class TestStorageModule {
 
-    // Makes Dagger provide FakeStorage when a Storage type is requested
-    @Binds
-    abstract fun provideStorage(storage: FakeStorage): Storage
 }
